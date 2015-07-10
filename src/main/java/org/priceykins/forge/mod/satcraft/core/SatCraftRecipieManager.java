2f149f470d5fd6c3fc39e556b2576f0a20bd1af2
Priceykins <priceykins@gmail.com>
2015-07-11 00:43:59 +0200

    Added recipies for titanium tools
    Added recipies for titanium:
    Sword
    Axe
    Shovel
    Hoe
    Pickaxe

--- src/main/java/org/priceykins/forge/mod/satcraft/core/SatCraftRecipieManager.java
+++ src/main/java/org/priceykins/forge/mod/satcraft/core/SatCraftRecipieManager.java
@@ -3,7 +3,9 @@ package org.priceykins.forge.mod.satcraft.core;
 import org.priceykins.forge.mod.satcraft.blocks.SatCraftBlocks;
 import org.priceykins.forge.mod.satcraft.items.SatCraftItems;
 
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.CraftingManager;
 import net.minecraft.item.crafting.FurnaceRecipes;
 
 public class SatCraftRecipieManager {
@@ -11,11 +13,22 @@ public class SatCraftRecipieManager {
 	public static void loadRecipies(){
 		
 		SatCraftRecipieManager.loadSmeltingRecipies();
+		SatCraftRecipieManager.loadToolRecipies();
 	}
 	
 	public static void loadSmeltingRecipies(){
+		
 		FurnaceRecipes.instance().addSmeltingRecipeForBlock(SatCraftBlocks.titaniumOre, new ItemStack(SatCraftItems.titaniumIngot, 1), 1.0F);
 		FurnaceRecipes.instance().addSmeltingRecipeForBlock(SatCraftBlocks.aluminiumOre, new ItemStack(SatCraftItems.aluminiumIngot, 1), 1.0F);
+	}
+	
+	public static void loadToolRecipies(){
+		
+		CraftingManager.getInstance().addRecipe(new ItemStack(SatCraftItems.titaniumPickaxe, 1), "ttt", " s ", " s ", Character.valueOf('s'), Items.stick, Character.valueOf('t'), SatCraftItems.titaniumIngot);
+		CraftingManager.getInstance().addRecipe(new ItemStack(SatCraftItems.titaniumSword, 1), " t ", " t ", " s ", Character.valueOf('s'), Items.stick, Character.valueOf('t'), SatCraftItems.titaniumIngot);
+		CraftingManager.getInstance().addRecipe(new ItemStack(SatCraftItems.titaniumAxe, 1), " tt", " st", " s ", Character.valueOf('s'), Items.stick, Character.valueOf('t'), SatCraftItems.titaniumIngot);
+		CraftingManager.getInstance().addRecipe(new ItemStack(SatCraftItems.titaniumShovel, 1), " t ", " s ", " s ", Character.valueOf('s'), Items.stick, Character.valueOf('t'), SatCraftItems.titaniumIngot);
+		CraftingManager.getInstance().addRecipe(new ItemStack(SatCraftItems.titaniumHoe, 1), "tt ", " s ", " s ", Character.valueOf('s'), Items.stick, Character.valueOf('t'), SatCraftItems.titaniumIngot);
 	}
 
 }
